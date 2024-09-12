9b092608d1a0dda6d6d9b5f16508fb392401cfc3
alexpra231 <54764447+alexpra231@users.noreply.github.com>
2024-09-12 18:42:04 +0300

    stuff

--- js/clicker.js
+++ js/clicker.js
@@ -1,6 +1,6 @@
 addLayer("c", {
     name: "Clicker", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "cl", // This appears on the layer's node. Default is the id with the first letter capitalized
+    symbol: "cl", // This appears on the layer's node. Default is the id with the first letter capitalized/
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
@@ -9,11 +9,12 @@ addLayer("c", {
     color: "green",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "levels", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    baseResource: "clicks", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
-    canBuyMax() {return !hasUpgrade("sp",11)},
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 2, // Prestige currency exponent
+    base: 2,
+    canBuyMax() {return hasUpgrade("sp",11)},
     branches:["p"],
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
@@ -32,7 +33,7 @@ addLayer("c", {
             display() {return "click"},
             canClick() {return "True"},
             onClick() {
-                return player.points=player.points.add(awnser)
+                return player.points=player.points.add(player[this.layer].points)
             }
         }
     }
--- js/mod.js
+++ js/mod.js
@@ -43,7 +43,7 @@ function getPointGen() {
 		return new Decimal(0)
 
 	let gain = new Decimal(1)
-	gain=gain.mul(100)
+	gain=gain.mul(0)
 	return gain
 }
 
